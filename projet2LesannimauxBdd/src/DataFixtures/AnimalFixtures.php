7064868f1bb8595d7b86a65b45c401199866b4de
ThomasPascual <thoma1@free.fr>
2020-04-24 11:28:10 +0200

    modification des fixtures(ajout dangereux et poids) et ajout dans la bdd

--- projet2LesannimauxBdd/src/DataFixtures/AnimalFixtures.php
+++ projet2LesannimauxBdd/src/DataFixtures/AnimalFixtures.php
@@ -14,20 +14,26 @@ class AnimalFixtures extends Fixture
         $a1->setNom("Chien")
         ->setDescription("un gentil toutout")
         ->setImage("chien.png")
+        ->setPoids(10)
+        ->setDangereux(false)
         ;
         $manager->persist($a1);
 
-        $a5 = new Animal();
-        $a5->setNom("Cochon")
+        $a2 = new Animal();
+        $a2->setNom("Cochon")
         ->setDescription("groin groin")
         ->setImage("cochon.png")
+        ->setPoids(30)
+        ->setDangereux(false)
         ;
-        $manager->persist($a5);
+        $manager->persist($a2);
 
         $a3 = new Animal();
         $a3->setNom("Serpent")
         ->setDescription("un animal dangereux")
         ->setImage("serpent.png")
+        ->setPoids(5)
+        ->setDangereux(true)
         ;
         $manager->persist($a3);
 
@@ -35,13 +41,17 @@ class AnimalFixtures extends Fixture
         $a4->setNom("Crocodile")
         ->setDescription("un animal très dangereux")
         ->setImage("croco.png")
+        ->setPoids(400)
+        ->setDangereux(true)
         ;
         $manager->persist($a4);
 
         $a5 = new Animal();
         $a5->setNom("Requin")
         ->setDescription("un animal marin très danger")
-        ->setImage("chien.png")
+        ->setImage("requin.png")
+        ->setPoids(700)
+        ->setDangereux(true)
         ;
         $manager->persist($a5);
 
