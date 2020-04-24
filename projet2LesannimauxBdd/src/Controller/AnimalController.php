89ca1d87ea7c992fee1357399942769fce4996cf
ThomasPascual <thoma1@free.fr>
2020-04-24 11:58:49 +0200

    ajout et lien avec une page pour un annimal

--- projet2LesannimauxBdd/src/Controller/AnimalController.php
+++ projet2LesannimauxBdd/src/Controller/AnimalController.php
@@ -20,4 +20,16 @@ class AnimalController extends AbstractController
             "lesAnimaux" => $animaux
         ]);
     }
+
+        /**
+     * @Route("/animal/{id}", name="afficher_animal")
+     */
+    public function afficherAnimal(AnimalRepository $repository ,$id)
+    {
+        $animal = $repository->find($id);
+
+        return $this->render('animal/afficherAnimal.html.twig',[
+            "unAnimal" => $animal
+        ]);
+    }
 }
